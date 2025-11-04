333da79b55530cb5682b14076e21cc2ad12e4780
Adam Zieliński <adam@adamziel.com>
2025-11-04 21:51:16 +0100

    initial commit

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Mock Plugin
+Description: A simple mock WordPress plugin for testing purposes.
+Version: 1.0.0
+Author: Your Name
+*/
+
+add_action( 'init', function() {
+    // Simple initialization logic can go here.
+} );
